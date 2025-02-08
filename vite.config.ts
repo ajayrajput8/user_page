7f774f4f8cd9cc0f7081bc342c39fad6b3ffb9dc
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-02-08 21:47:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [react()],
 
   optimizeDeps: {
@@ -15,4 +15,24 @@ export default defineConfig({
       external: ["firebase"]
     }
   }
+});*/
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["firebase/firestore"],
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Separates dependencies
+          }
+        },
+      },
+    },
+  },
 });
+
