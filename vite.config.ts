dd36bbd2a5d8dc7174691c34865301458b888d6d
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-02-08 21:36:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ["firebase/firestore"],
     exclude: ['lucide-react'],
   },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; // Separates dependencies from your app code
-          }
-        },
-      },
-    }
-  }
 });
