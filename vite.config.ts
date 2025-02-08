21960263c437a45e558c4c63cb1c0cafde313d56
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-02-08 22:21:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
+
+/*import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
@@ -21,7 +32,7 @@ export default defineConfig({
   },
 });
 
-/*import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
  https://vitejs.dev/config/
