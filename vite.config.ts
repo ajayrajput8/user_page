b9b9c11688578a0b543c2db0e35229eff12a3415
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-02-08 21:59:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
-// https://vitejs.dev/config/
-/*export default defineConfig({
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [commonjs()],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['firebase'], // Optional, if issues persist
+  },
+});
+
+/*import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+ https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
 
   optimizeDeps: {
@@ -15,7 +31,7 @@ import react from '@vitejs/plugin-react';
       external: ["firebase"]
     }
   }
-});*/
+});
 
 export default defineConfig({
   plugins: [react()],
@@ -34,5 +50,5 @@ export default defineConfig({
       },
     },
   },
-});
+});*/
 
