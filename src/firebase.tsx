9bfad12f2e745cd88202d0635878e2684d6ba5a6
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-02-19 20:06:10 +0530

    Update firebase.tsx

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,15 +1,16 @@
 import {getFirestore} from 'firebase/firestore'
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCzFW_-M45FReyScJ3zYUdf3PcTAgFwVx4",
-    authDomain: "sabkimandi-1b2bc.firebaseapp.com",
-    projectId: "sabkimandi-1b2bc",
-    storageBucket: "sabkimandi-1b2bc.firebasestorage.app",
-    messagingSenderId: "907398008774",
-    appId: "1:907398008774:web:ad4153a3b64bb526016cc7",
-    measurementId: "G-5T9LN6EXK2"
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   };
+  
 
   const app=initializeApp(firebaseConfig)
   const db=getFirestore(app)
