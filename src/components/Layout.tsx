27e3f3a71cc14418333ac17d8064a85001e5384f
ajayrajput8 <singhrajputajay2004@gmail.com>
2025-02-23 15:43:34 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,7 +17,7 @@ export function Layout({ children, cartItemsCount = 0, onCartClick}: LayoutProps
     <div className="min-h-screen bg-gray-50">
       <header className="sticky top-0 bg-white shadow-md z-50 bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
-          <button className="text-2xl font-bold text-emerald-600" onClick={()=>setShowProfile(false)}>SAB KI MANDI</button>
+          <button className="text-2xl font-bold text-emerald-600" onClick={()=>setShowProfile(false)}>THUGKART</button>
           <div className='flex items-center gap-6'>
               <button 
                 onClick={onCartClick}
